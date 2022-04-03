01e52db40a99a7e310a17a8c2098c2391afee373
marijamaki26@gmail.com <marijamaki26@gmail.com>
2022-04-03 23:02:12 +0200

    Inicijalizacija projekta

---
+++ src/main/java/ftn/mrs/isa/rentalapp/RentalAppApplication.java
@@ -0,0 +1,13 @@
+package ftn.mrs.isa.rentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ftn/mrs/isa/rentalapp/RentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.mrs.isa.rentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
