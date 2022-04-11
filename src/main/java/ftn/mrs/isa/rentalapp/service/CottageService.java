3f88a39573ca305c9663804f951656a7f5208998
marijamaki26@gmail.com <marijamaki26@gmail.com>
2022-04-11 14:55:07 +0200

    5.2.2 Implementacija servisne metode za prikaz vikendice

---
+++ src/main/java/ftn/mrs/isa/rentalapp/service/CottageService.java
@@ -0,0 +1,19 @@
+package ftn.mrs.isa.rentalapp.service;
+
+import ftn.mrs.isa.rentalapp.model.entity.Cottage;
+import ftn.mrs.isa.rentalapp.repository.CottageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CottageService {
+
+    @Autowired
+    private CottageRepository cottageRepository;
+
+    public List<Cottage> findAll(){return cottageRepository.findAll();}
+
+    public Cottage findOne(Integer id){return cottageRepository.findById(id).orElse(null);}
+}
