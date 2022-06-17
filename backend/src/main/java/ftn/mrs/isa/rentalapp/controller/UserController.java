8a44a33c9cd03196d8b2079a7766ad340da69073
Ivana Kasikovic <ikasikovic@yahoo.com>
2022-06-17 17:39:10 +0200

    72.2 Kreirane backend funkcije za mijenjanje lozinke kod admina

--- backend/src/main/java/ftn/mrs/isa/rentalapp/controller/UserController.java
+++ backend/src/main/java/ftn/mrs/isa/rentalapp/controller/UserController.java
@@ -16,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
