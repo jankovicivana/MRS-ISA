5196644dc3a3c0327a2553f40709ad463edc0231
Ivana Jankovic <ivanaj0610@gmail.com>
2022-06-22 00:06:04 +0200

    Promijenili vue_app_server_port

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_SERVER_PORT : '"http://localhost:9000"',
+  VUE_APP_SERVER_PORT : '"https://rental-app-6-front.herokuapp.com/"',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
