9d592b338de304f8c6da7fcfa994a2cf69024dc1
Ivana Jankovic <ivanaj0610@gmail.com>
2022-06-22 00:11:21 +0200

    Opet popravljeno

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_SERVER_PORT : '"https://rental-app-6-front.herokuapp.com/"',
+  VUE_APP_SERVER_PORT : '"https://rental-app-6.herokuapp.com/"',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
