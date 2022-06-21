584963801f42382bebebcebc207b9c58041a48b2
Ivana Jankovic <ivanaj0610@gmail.com>
2022-06-22 00:46:51 +0200

    Ajde vise

--- backend/src/main/java/ftn/mrs/isa/rentalapp/config/WebConfig.java
+++ backend/src/main/java/ftn/mrs/isa/rentalapp/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:4000")
+        registry.addMapping("/**").allowedOrigins("https://rental-app-6-front.herokuapp.com")
                 .allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
 
     }
