ba23c1407d3f627c6ba9f54b533b9dfccee0c5a5
Ivana Jankovic <ivanaj0610@gmail.com>
2022-06-22 20:33:15 +0200

    Pokusaj da se popravi webconfig

--- backend/src/main/java/ftn/mrs/isa/rentalapp/config/WebConfig.java
+++ backend/src/main/java/ftn/mrs/isa/rentalapp/config/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
-                .allowedHeaders("*").allowedOrigins("https://rental-app-6-front.herokuapp.com");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
 
     }
 }
